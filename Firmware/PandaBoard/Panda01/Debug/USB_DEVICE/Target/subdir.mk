5af8e86e2bff34ce7a0b54b2c339e5543be0f8b9
jvsanches <syxtroz@gmail.com>
2021-06-12 00:17:03 -0300

    add device configuration for board version 1.0

--- Firmware/PandaBoard/Panda01/Debug/USB_DEVICE/Target/subdir.mk
+++ Firmware/PandaBoard/Panda01/Debug/USB_DEVICE/Target/subdir.mk
@@ -1,5 +1,6 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
@@ -14,6 +15,6 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-USB_DEVICE/Target/usbd_conf.o: ../USB_DEVICE/Target/usbd_conf.c
+USB_DEVICE/Target/usbd_conf.o: ../USB_DEVICE/Target/usbd_conf.c USB_DEVICE/Target/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F072xB -DDEBUG -c -I../Core/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -I../USB_DEVICE/App -I../USB_DEVICE/Target -I../Middlewares/ST/STM32_USB_Device_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"USB_DEVICE/Target/usbd_conf.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
